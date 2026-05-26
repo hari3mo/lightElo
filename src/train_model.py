5773eef3aa3b1f5a3f17dc288da58a02d1de36b8
hari3mo <harisasaif@gmail.com>
2026-05-25 23:59:38 -0700

    train

--- src/train_model.py
+++ src/train_model.py
@@ -1,4 +1,6 @@
+from sklearn.metrics import mean_absolute_error
 import pandas as pd
+import lightgbm
 
 FEATURES_PATH = 'data/lichess_features.csv' # output from create_features.py
 OUTPUT_PATH = 'models/lightgbm.txt' # export trained model
@@ -27,6 +29,23 @@ def main():
     X_tr, y_tr = train_df[features], train_df['elo']
     X_va, y_va = val_df[features], val_df['elo']
     X_te, y_te = test_df[features], test_df['elo']
+
+    # Train LightGBM model
+    tr_set = lightgbm.Dataset(X_tr, y_tr, categorical_feature=cat_cols)
+    va_set = lightgbm.Dataset(X_va, y_va, categorical_feature=cat_cols, reference=tr_set)
+    model = lightgbm.train(
+    {'objective': 'regression', 'metric': 'mae', 'seed': 42, 'verbose': -1},
+    train_set=tr_set,
+    num_boost_round=2000,
+    valid_sets=[va_set],
+    callbacks=[lightgbm.early_stopping(100)]
+)
+
+    preds = model.predict(X_te)
+    mae = mean_absolute_error(y_te, preds)
+    print(f'Test MAE: {mae}')
+
+    model.save_model(OUTPUT_PATH)
 
 if __name__ == "__main__":
     main()
