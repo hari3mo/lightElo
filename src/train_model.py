e944483a5e495f0a2948c0687873f85e3b9c6666
hari3mo <harisasaif@gmail.com>
2026-05-25 23:52:12 -0700

    split

--- src/train_model.py
+++ src/train_model.py
@@ -5,13 +5,28 @@ OUTPUT_PATH = 'models/lightgbm.txt' # export trained model
 
 def main():
     df = pd.read_csv(FEATURES_PATH)
+    cat_cols = ['eco', 'category', 'is_white']
+    num_cols = ['acpl', 'blunders', 'mistakes', 'inaccuracies', 'avg_move_time',
+                'time_trouble_moves', 'opening_speed', 'shift_move_time',
+                'ply_count', 'eval_volatility']
+    features = num_cols + cat_cols
+    for col in cat_cols:
+        df[col] = df[col].astype('category')
+
+    # Split data on unique game IDs to avoid leakage between training and validation/test sets
     games = df['game_id'].drop_duplicates().values
     n = len(games)
-    tr, va = set(games[:int(n*0.8)]), set(games[int(n*0.8):]) # split data 80 / 10 into training and validation sets
-    te = set(games[int(n*0.9):]) # use last 10% of data as test set
+    tr = set(games[:int(n*0.8)]) 
+    va = set(games[int(n*0.8):]) 
+    te = set(games[int(n*0.9):]) 
+
     train_df = df[df['game_id'].isin(tr)]
     val_df = df[df['game_id'].isin(va)]
     test_df = df[df['game_id'].isin(te)]
+
+    X_tr, y_tr = train_df[features], train_df['elo']
+    X_va, y_va = val_df[features], val_df['elo']
+    X_te, y_te = test_df[features], test_df['elo']
 
 if __name__ == "__main__":
     main()
